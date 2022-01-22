589f9cb64e31a5952f64ab8e503c098410ce12c6
JoosangKim <fable0831@gmail.com>
2022-01-23 00:28:57 +0900

    상세 페이지
    지도 아직 못함

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -17,6 +17,7 @@ const DetailPage = () => {
   // kinder 객체 안에 detail 정보 있습니다.
   const [kinder, setKinder] = useState({});
 
+   
   useEffect(() => {
     const payload = dispatch(getDetailPage(kinder_id)).payload;
     payload.then((data) => {
@@ -31,19 +32,57 @@ const DetailPage = () => {
 
   return (
     <div className="detail">
-      <script
-        type="text/javascript"
-        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=346d4bb4e554014036caf8ecde259a0f"
-      ></script>
       <div className="detail-header">
         <h5>유치원 및 어린이집 세부 정보</h5>
       </div>
       <div className="detail-info">
         <div className="detail-left-box">
           <div className="detail-kinder-header">
+            <div className="icon">
+                <img className="babyicon" alt="baby" src="img/babyicon.png" />
+            </div>
             <h4>{kinder.kinder_name}</h4>
           </div>
-          <button onClick={handleHomePage}>Home</button>
+          <button className="minihompage-button" onClick={handleHomePage}>미니홈페이지 바로가기</button>
+        </div>
+        <div className="detail-info-list">
+          <div>
+            <ul className="detail-list">
+              <li>
+                <i className="detail-list-menu1">기본 정보</i>
+                <span className="detail-list-contents"></span>
+              </li>
+              <li>
+                <i className="detail-list-menu">전화번호</i>
+                <span className="detail-list-contents">{kinder.tel_no}</span>
+              </li>
+              <li>
+                <i className="detail-list-menu">운영시간</i>
+                <span className="detail-list-contents">{kinder.oper_time}</span>
+              </li>
+              <li>
+                <i className="detail-list-menu">원장명</i>
+                <span className="detail-list-contents">{kinder.ldgr_name}</span>
+              </li>
+              <li>
+                <i className="detail-list-menu">주소</i>
+                <span className="detail-list-contents">{kinder.addr}</span>
+              </li>
+              <li>
+                <i className="detail-list-menu">홈페이지</i>
+                <span className="detail-list-contents">{kinder.hp_addr}</span>
+              </li>
+              <li>
+                <i className="detail-list-menu1">통학차량</i>
+                <span className="detail-list-contents">{kinder.vhcl_oprn_yn}</span>
+              </li>
+              <li>
+                <i className="detail-list-menu1">차량 운행 대수</i>
+                <span className="detail-list-contents">{kinder.opra_vhcnt}</span>
+              </li>
+            </ul>
+          </div>
+          <div id="myMap"></div> 
         </div>
       </div>
     </div>
